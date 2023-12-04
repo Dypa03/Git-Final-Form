075f64e5f7cbc6d1e9ac27510a993aff9c186ccb
Dypa03 <davidedipaola.03.official@gmail.com>
2023-12-04 12:38:27 +0100

    Aggiunzione delle classi Dipendenze

---
+++ src/com/main/MiaDipendenza.java
@@ -0,0 +1,17 @@
+package com.main;
+
+public class MiaDipendenza {
+
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
